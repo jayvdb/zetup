4fa1e552999aeee2d6dc483cdbb3f8ee77baab60
Stefan Zimmermann <zimmermann.code@gmail.com>
2018-07-22 18:35:35 +0200

    requires: CleanUp formatting and docstrings

--- zetup/requires.py
+++ zetup/requires.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with zetup.py. If not, see <http://www.gnu.org/licenses/>.
 
-__all__ = ['Requirements', 'DistributionNotFound', 'VersionConflict']
+__all__ = ('Requirements', 'DistributionNotFound', 'VersionConflict')
 
 import sys
 if sys.version_info[0] == 3:
@@ -25,11 +25,12 @@ if sys.version_info[0] == 3:
 import re
 
 import pkg_resources
-from pkg_resources import \
-    parse_requirements, Requirement, get_distribution
+from pkg_resources import (
+    parse_requirements, Requirement, get_distribution)
 
 
 class DistributionNotFound(pkg_resources.DistributionNotFound):
+
     def __init__(self, req, requirer, reason=None):
         super(DistributionNotFound, self).__init__(req, [requirer])
         self.requirer = requirer
@@ -43,34 +44,39 @@ class DistributionNotFound(pkg_resources.DistributionNotFound):
 
 
 class VersionConflict(pkg_resources.VersionConflict):
+
     def __init__(self, req, found_version, requirer, reason=None):
         super(VersionConflict, self).__init__(
-          '%s-%s' % (req.key, found_version), req)
+            '%s-%s' % (req.key, found_version), req)
         self.requirer = requirer
         self.reason = reason
 
     def __str__(self):
         text = "%s needs %s but found %s" % (
-          self.requirer, self.req, self.dist)
+            self.requirer, self.req, self.dist)
         if self.reason:
             text += " (%s)" % self.reason
         return text
 
 
 class Requirements(object):
-    """Package requirements manager.
-    """
+    """Package requirements manager."""
+
     @staticmethod
     def _parse(text):
-        """ Generate parsed requirements from `text`,
-            which should contain newline separated requirement specs.
+        """
+        Generate parsed requirements from `text`.
+        
+        :param text:
+            Newline-separated requirement specs
 
-        - Additionally looks for "#import name" comments
-          after requirement lines (the actual root module name
-          of the required package to use for runtime dependency checks)
-          and stores them as .impname attrs on the Requirement instances.
-        - Supports #py.. tags at the beginning of lines,
-          specifying a python version the requirement applies to.
+        Additionally looks for ``#import name`` comments after requirement
+        lines (the actual root module name of the required package to use for
+        runtime dependency checks) and stores them as .impname attrs on the
+        Requirement instances.
+
+        Supports ``#py..`` tags at the beginning of lines, specifying a python
+        version the requirement applies to.
         """
         for line in text.split('\n'):
             line = line.strip()
@@ -96,16 +102,17 @@ class Requirements(object):
             yield req
 
     def __init__(self, reqs, zfg=None):
-        """Store a list of :class:`pkg_resources.Requirement` instances
-           from the given requirement specs
-           and additionally store them newline separated
-           in the :class:`str` base.
+        """
+        Generate a list of :class:`pkg_resources.Requirement` instances.
 
-        :param reqs: Either a single string of requirement specs
-          or a sequence of strings and/or
-          :class:`pkg_resources.Requirement` instances.
-        :param zfg: Optional zetup config object
-          the requirements are related to.
+        And additionally store all requirement specs as text with newline
+        separators in the ``str`` base
+
+        :param reqs:
+            Either a single string of requirement specs or a sequence of
+            strings and/or :class:`pkg_resources.Requirement` instances
+        :param zfg:
+            Optional zetup config object the requirements are related to
         """
         if isinstance(reqs, Requirements):
             txt = reqs.txt
@@ -130,35 +137,51 @@ class Requirements(object):
         self.zfg = zfg
 
     def __eq__(self, other):
-        return isinstance(other, Requirements) \
-          and self._list == other._list
+        return isinstance(other, Requirements) and (
+            self._list == other._list)
 
     def __str__(self):
         return '\n'.join(map(str, self))
 
-    def check(self, raise_=True):
-        """Check that all requirements are available (importable)
-           and their versions match (using modules' __version__ attributes).
-
-        - Fallback to pkg_resources.get_distribution().version
-          if no __version__ or is None.
-
-        :param raise_: Raise DistributionNotFound if ImportError
-          or VersionConflict if version doesn't match?
-          If False just return False in that case.
+    def check(self, requirer=None, raise_=True):
         """
-        requirer = self.zfg and '%s-%s' % (
-          self.zfg.NAME, self.zfg.VERSION or '(none)')
+        Check that all requirements are satisfied.
+        
+        They must be importable and their versions (from their top-level
+        package modules' ``.__version__`` attributes) must match the required
+        specs
+
+        Falls back to ``pkg_resources.get_distribution().version`` if
+        ``.__version__`` is missing or ``None``
+
+        :param requirer:
+            Optional identification string of the requirer
+        :param raise_:
+            If set to ``False`` then also just return ``False`` on unsatisfied
+            requirements instead of raising exceptions
+
+        :raises zetup.DistributionNotFound:
+            If a requirement can't be imported
+        :raises zetup.VersionConflict:
+            If a required version doesn't match
+        """
+        if requirer is None:
+            requirer = self.zfg and '%s-%s' % (
+                self.zfg.NAME, self.zfg.VERSION or '(none)')
         for req in self:
             try:
                 mod = __import__(req.impname)
             except ImportError as e:
                 if raise_:
-                    raise DistributionNotFound(req, requirer,
-                      reason="%s: %s" % (type(e).__name__, e))
+                    raise DistributionNotFound(
+                        req, requirer,
+                        reason="%s: %s" % (type(e).__name__, e))
+
                 return False
+
             if not req.specs: # No version constraints
                 continue
+
             try:
                 version = mod.__version__
                 if version is None:
@@ -166,21 +189,27 @@ class Requirements(object):
                     # (handle in following except block)
                     # ==> same exception, just different msg
                     raise AttributeError(
-                      "module's '__version__' attribute is None")
+                        "module's '__version__' attribute is None")
+
             except AttributeError as e_no__version__attr:
                 try: # try to get version from distribution
                     dist = get_distribution(req.key)
                 except pkg_resources.DistributionNotFound as e:
                     if raise_:
-                        raise VersionConflict(req, None, requirer,
-                          reason="%s: %s. %s: %s" % (
-                            e_no__version__attr, mod, type(e).__name__, e))
+                        raise VersionConflict(
+                            req, None, requirer,
+                            reason="%s: %s. %s: %s" % (
+                                e_no__version__attr, mod, type(e).__name__, e))
+
                     return False
+
                 version = dist.version
             if version not in req:
                 if raise_:
                     raise VersionConflict(req, version, requirer)
+
                 return False
+
         return True
 
     @property
@@ -192,25 +221,28 @@ class Requirements(object):
         return iter(self._list)
 
     def __getitem__(self, name):
-        """Get a requirement by its distribution name.
-        """
+        """Get a requirement by its distribution name."""
         for req in self._list:
             if name in [req.key, req.unsafe_name]:
                 return req
+
         raise KeyError(name)
 
     def __delitem__(self, name):
-        """Delete a requirement by its distribution name.
-        """
+        """Delete a requirement by its distribution name."""
         for req in list(self._list):
             if name in [req.key, req.unsafe_name]:
                 return self._list.remove(req)
+
         raise KeyError(name)
 
     def __add__(self, reqs):
-        """Return a new :class:`Requirements` instance
-           with additional `reqs` from string
-           or another :class:`Requirements` instance.
+        """
+        Create a new :class:`Requirements` instance.
+
+        :param reqs:
+            Additional requirements as string or another :class:`Requirements`
+            instance
         """
         if isinstance(reqs, Requirements):
             reqs = reqs.txt
@@ -223,8 +255,8 @@ class Requirements(object):
     @property
     def py(self):
         return '%s("""\n%s\n""", zfg=zfg)' % (
-          type(self).__name__, self.txt) ## '\n'.join(
-            ## '%s #import %s' % (req, req.impname) for req in self))
+            type(self).__name__, self.txt)  # '\n'.join(
+            # '%s #import %s' % (req, req.impname) for req in self))
 
     def __repr__(self):
         return str(self)
