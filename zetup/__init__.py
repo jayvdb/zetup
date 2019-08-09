547718301384753aef29a52fc22e110bf6bffbcb
Stefan Zimmermann <user@zimmermann.co>
2019-08-09 20:25:24 +0200

    Move setup_entry_point() to sub-module

--- zetup/__init__.py
+++ zetup/__init__.py
@@ -31,6 +31,7 @@ pkg_resources.iter_entry_points = pkg_resources.WorkingSet().iter_entry_points
 pkg_resources.require = pkg_resources.WorkingSet().require
 
 
+from .entry_point import setup_entry_point
 from .zetup import Zetup, find_zetup_config
 from .error import ZetupError
 from .config import ZetupConfigNotFound
@@ -49,6 +50,7 @@ from . import notebook
 
 
 zetup = toplevel(__name__, [
+    'setup_entry_point',
     'Zetup', 'find_zetup_config',
     'ZetupError', 'ZetupConfigNotFound',
     'ZFG_PARSER', 'ZFGFile', 'ZFGData', 'parse_zfg',
@@ -77,50 +79,3 @@ extra_toplevel(zetup, notebook.__name__, [
 # a manually setting of the subpackage attribute
 # on the zetup toplevel wrapper is needed
 sys.modules[__name__].notebook = sys.modules[notebook.__name__]
-
-
-def setup_entry_point(dist, keyword='use_zetup', value=True):
-    """
-    Zetup's ``entry_point`` handler for the ``setup()`` process in a project's
-    **setup.py**, setting all setup keyword parameters based on zetup config
-
-    Activated with ``setup(setup_requires=['zetup'], use_zetup=True)``
-    """
-    assert keyword == 'use_zetup'
-    if not value:
-        return
-
-    from .zetup import Zetup
-    # read config from current working directory (where setup.py is run)
-    zfg = Zetup()
-    keywords = zfg.setup_keywords()
-    for name, value in keywords.items():
-        # generally, setting stuff on dist.metadata is enough (and necessary)
-        setattr(dist.metadata, name, value)
-        # but *pip* only works correct if some stuff is also set directly on
-        # dist object (pip seems to read at least package infos somehow from
-        # there)
-        if name.startswith('package') or name.endswith('requires'):
-            setattr(dist, name, value)
-
-    if zfg.in_repo:
-        # resolve requirements for zetup make
-        resolve(['zetup[commands]>={}'.format(
-            __import__('zetup').__version__)])
-        import zetup.commands as _
-
-        # make necessary files and store make result in distribution object,
-        # so that files can be removed by del dist.zetup_made after setup()
-        make_targets = ['VERSION', 'setup.py', 'zetup_config']
-        dist.zetup_made = zfg.make(targets=make_targets)
-
-    # finally run any custom setup hooks defined in project's zetup config
-    if zfg.SETUP_HOOKS:
-        sys.path.insert(0, '.')
-        for hook in zfg.SETUP_HOOKS:
-            modname, funcname = hook.split(':')
-            mod = __import__(modname)
-            for subname in modname.split('.')[1:]:
-                mod = getattr(mod, subname)
-            func = getattr(mod, funcname)
-            func(dist)
